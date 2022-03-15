a7296fa48d376eb5c3c8c4a82b38c0f0cb6dc094
evgenkolesman <glavstroi_e@mail.ru>
2022-03-15 12:17:58 +0300

    Updating projects for messaging about services

--- src/main/java/ru/koleson/apigateway/filter/AuthHeaderFilter.java
+++ src/main/java/ru/koleson/apigateway/filter/AuthHeaderFilter.java
@@ -1,25 +1,25 @@
 package ru.koleson.apigateway.filter;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.impl.TextCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.*;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.logging.Logger;
 
 @Component
 public class AuthHeaderFilter extends AbstractGatewayFilterFactory<AuthHeaderFilter.Config> {
+
+    Logger logger = Logger.getLogger(String.valueOf(AuthHeaderFilter.class));
 
     @Autowired
     Environment environment;
@@ -43,9 +43,10 @@ public class AuthHeaderFilter extends AbstractGatewayFilterFactory<AuthHeaderFil
             }
 
             String authHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-            String jwt = authHeader.replace("Bearer ", "");
+            String jwt = authHeader.replace("Bearer ", "").replace(" ", "");
 
             if(!isJwtValid(jwt)) {
+                logger.info("Problems with token " + jwt);
                 return  onError(exchange, "JWT token is not valid",
                         HttpStatus.UNAUTHORIZED);
             }
@@ -80,15 +81,15 @@ public class AuthHeaderFilter extends AbstractGatewayFilterFactory<AuthHeaderFil
                     .getBody()
                     .getSubject();
         } catch (Exception e) {
+            e.printStackTrace();
+            logger.info(sub.getBytes(StandardCharsets.UTF_8).toString());
             returnVal = false;
         }
 
         if(sub == null || sub.isEmpty()) {
             returnVal = false;
         }
-//        if(jwt == null || jwt.isEmpty()) {
-//            returnVal = false;
-//        }
+
         return returnVal;
     }
 
